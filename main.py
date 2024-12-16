d77fb7b89e00b8b5ab50eb1ebb038999352a7cb4
Jimmy9812 <wladimirmaila1998pm@gmail.com>
2024-12-15 19:32:18 -0500

    feat: :sparkles: update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Docker! con Jimmy Maila"
+    return "Hello, Docker! con Jimmy Maila prueba 1"
 
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=5000, debug=True)
